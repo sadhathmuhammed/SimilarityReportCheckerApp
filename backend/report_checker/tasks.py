8c544a6dec35e7974ae2b1ddd7f6f19dcf38d3bd
Muhammed Sadhath <muhammed.sadhath@scrapehero.com>
2025-07-12 13:29:35 +0530

    email formatted

--- backend/report_checker/tasks.py
+++ backend/report_checker/tasks.py
@@ -17,17 +17,27 @@ def send_submission_email(submission_version_id):
     faculty = version.submission.student.faculty
 
     message = f"""
-    New submission by {version.submission.student.name}
-    Timestamp: {version.upload_timestamp}
-    Similarity: {version.similarity_percentage}%
-    Files:
-    DOCX: {version.docx_file.url}
-    PPTX: {version.ppt_file.url}
-    Report: {version.similarity_report.url}
+    Hello {version.submission.student.faculty.name}
+    
+    Student {version.submission.student.name} has submitted an assignment with the following details:
+
+    - Similarity: {version.similarity_percentage}%
+    - Timestamp: {version.upload_timestamp}
+    - Files:
+        DOCX: {version.docx_file.url}
+        PPTX: {version.ppt_file.url}
+        Report: {version.similarity_report.url}
+        
+    Please log in to the faculty dashboard to review the full submission.
+
+    Regards,
+    Assignment Portal
+
+
     """
 
     send_mail(
-        subject=f"New Submission from {version.submission.student.name}",
+        subject=f"Assignment Submission from {version.submission.student.name} – Similarity: {version.similarity_percentage}%",
         message=message,
         from_email=settings.DEFAULT_FROM_EMAIL,
         recipient_list=[faculty.email]
